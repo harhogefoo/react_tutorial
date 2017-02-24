6c14096ba4df7c559c5837c4e70666a468e76503
harhogefoo <harimanmon@gmail.com>
2017-02-25 04:44:58 +0900

    Init commit

---
+++ js/state_and_lifecycle.js
@@ -0,0 +1,60 @@
+// function Clock(props) {
+//   return (
+//       <div>
+//         <h1>Hello, world!</h1>
+//         <h2>It is {props.date.toLocaleTimeString()}.</h2>
+//       </div>
+//   );
+// }
+
+function FormattedDate(props) {
+  return <h2>It is {props.date.toLocaleTimeString()}.</h2>;
+}
+
+class Clock extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {date: new Date()};
+  }
+
+  componentDidMount() {
+    this.timeID = setInterval(
+        () => this.tick(),
+        1000
+    );
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.timerID);
+  }
+
+  tick() {
+    this.setState({
+      date: new Date()
+    });
+  }
+
+  render() {
+    return (
+        <div>
+          <h1>Hello, world!</h1>
+          <FormattedDate date={this.state.date} />
+        </div>
+    );
+  }
+}
+
+function App() {
+  return (
+      <div>
+        <Clock />
+        <Clock />
+        <Clock />
+      </div>
+  );
+}
+
+ReactDOM.render(
+    <App />,
+    document.getElementById('content')
+);
